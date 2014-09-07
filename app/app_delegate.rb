88c4a645bda489fea9db0adc82f16f81a8e02173
Erik Nilsen <enilsen16@live.com>
2014-09-06 17:43:09 -0700

    add inital hello world popup

---
+++ app/app_delegate.rb
@@ -0,0 +1,12 @@
+class AppDelegate
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    alert = UIAlertView.new
+    alert.message = "Hello World!"
+    alert.show
+
+    puts "Hello again!"
+
+    
+    true
+  end
+end
